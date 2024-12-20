045194e7f2b70082f9a72686a197f95de5d8dad0
JoaoDiaas12 <jvitor.1212@hotmail.com>
2024-12-20 00:36:16 -0300

    3 finalizado

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-index.js
+class hero{
+	constructor(tipo, ataque, nome, idade){
+		this.tipo = tipo
+        this.ataque = ataque
+        this.nome = nome
+        this.idade = idade
+	}
+    
+    escrever(){
+    	console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+        console.log("O bravo guerreiro de nome " + this.nome + " tem " + this.idade + " anos")
+    }     
+}
+
+let saida = new hero("Guerreiro", "a espada", "Jovir", "24")
+saida.escrever()
